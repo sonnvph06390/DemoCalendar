88c54a20496e709542a04e999eccb1a030cb184c
sơn nguyễn <sonnguyen106mh@gmail.com>
2019-10-11 16:09:29 +0700

    Update click

--- app/src/main/java/com/example/son/democalendar/MainActivity.java
+++ app/src/main/java/com/example/son/democalendar/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.haibin.calendarview.Calendar;
 import com.haibin.calendarview.CalendarLayout;
@@ -14,9 +16,12 @@ import com.haibin.calendarview.CalendarView;
 
 import java.util.List;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements CalendarView.OnCalendarSelectListener,
+        View.OnClickListener {
     private CalendarLayout calendarLayout;
     private CalendarView calendarView;
+    private TextView tvToday;
+    private TextView tvLunarToday;
 
     @Override
     protected int getLayoutId() {
@@ -27,8 +32,15 @@ public class MainActivity extends BaseActivity {
     protected void initView() {
         calendarLayout = (CalendarLayout) findViewById(R.id.calendarLayout);
         calendarView = (CalendarView) findViewById(R.id.calendarView);
-
-
+        tvToday = (TextView) findViewById(R.id.tvToday);
+        tvLunarToday = findViewById(R.id.tvLunarToday);
+        tvToday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        tvToday.setText(" Dương lịch " + calendarView.getCurDay() + " tháng " + calendarView.getCurMonth() + " năm " + calendarView.getCurYear());
+        calendarView.setOnCalendarSelectListener(this);
     }
 
     @Override
@@ -36,4 +48,33 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    @Override
+    public void onCalendarOutOfRange(Calendar calendar) {
+
+    }
+
+    @Override
+    public void onCalendarSelect(Calendar calendar, boolean isClick) {
+        tvToday.setText("Dương lịch " + calendar.getDay() + " tháng " + calendar.getMonth() + " năm " + calendar.getYear());
+        tvLunarToday.setText("Âm lịch " + calendar.getLunarCalendar().getDay() + " tháng " + calendar.getLunarCalendar().getMonth() + " năm " +
+                calendar.getLunarCalendar().getYear());
+        if (isClick) {
+            Toast.makeText(this, getCalendarText(calendar), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    private static String getCalendarText(Calendar calendar) {
+        return String.format("Lịch:%s \n Âm lịch %s \n Dương lịch：%s \n Tết âm lịch：%s \n Tiết khí：%s \n 是否闰月：%s",
+                calendar.getMonth() + "ngày " + calendar.getDay() + " tháng",
+                 "ngày " + calendar.getLunarCalendar().getDay() + " tháng "+calendar.getLunarCalendar().getMonth(),
+                TextUtils.isEmpty(calendar.getGregorianFestival()) ? "Không" : calendar.getGregorianFestival(),
+                TextUtils.isEmpty(calendar.getTraditionFestival()) ? "Không" : calendar.getTraditionFestival(),
+                TextUtils.isEmpty(calendar.getSolarTerm()) ? "Không" : calendar.getSolarTerm(),
+                calendar.getLeapMonth() == 0 ? "Không" : String.format("Nhuận %s tháng", calendar.getLeapMonth()));
+    }
 }
